481236e217f473a456881ec70c85cdf1cbe13327
Sachin Darji <sdarji@gmail.com>
2014-07-28 13:51:12 -0500

    completed exercise 02

---
+++ ex/ex02.py
@@ -0,0 +1,12 @@
+# LPTHW Exercise 2
+
+# a comment
+
+print('I could have code like this.') # another comment
+
+# print('This won\'t run.')
+
+print('I don\'t know if this will work.')
+
+print('This will run.')
+
