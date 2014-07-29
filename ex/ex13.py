0d04d2f14bc54ee2bd7c50aaafb72200dab9288a
Sachin Darji <sdarji@gmail.com>
2014-07-29 00:14:16 -0500

    Experiment with sys.argv

--- ex/ex13.py
+++ ex/ex13.py
@@ -2,9 +2,11 @@
 
 from sys import argv
 
-script, first, second, third = argv
+#script, first, second, third = argv
+
+script = argv        # see if this will give me name of my script
 
 print "The script is called:", script
-print "Your first variable is called:", first
-print "Your second variable is called:", second
-print "Your third variable is called:", third
+#print "Your first variable is called:", first
+#print "Your second variable is called:", second
+#print "Your third variable is called:", third
