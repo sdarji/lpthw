09abf7cfe74bb19452bcf2d1051078736512e557
Sachin Darji <sdarji@gmail.com>
2014-07-28 20:36:47 -0500

    complete exercise 5 including drills

--- ex/ex05.py
+++ ex/ex05.py
@@ -1,20 +1,21 @@
 # LPTHW Exercise 5 -- More Variables and Printing
 
-my_name = 'Zed A. Shaw'
-my_age = 35 # not a lie
-my_height = 74 # inches
-my_weight = 180 # lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 * 2.54 # cm
+weight = 180 / 2.2 # kgs
+eyes = 'Blue\\'
+teeth = 'White'
+hair = 'Brown\\'
+num_teeth = 'two'
 
-print "Let's talk about %s." % my_name
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
+print "Let's talk about %s." % name
+print "He's %d centimeters tall." % height
+print "He's %d kilograms heavy." % weight
 print "Actually that's not too heavy."
-print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
-print "His teeth are usually %s depending on the coffee." % my_teeth
+print "He's got %s eyes and %r hair." % (eyes, hair)
+print "His %r teeth are usually %s depending on the coffee." % (num_teeth, teeth)
 
 # this line is tricky, try to get it exactly right
-print('If I add %d, %d, and %d, I get %d.' % (my_age, my_height, 
-       my_weight, my_age + my_height + my_weight))
+print('If I add %d, %d, and %d, I get %d.' % (age, height, 
+      weight, age + height + weight))
