d6dcbc1cc92f1e682a4169212d44c51e3c3c9a48
Sachin Darji <sdarji@gmail.com>
2014-07-29 00:12:18 -0500

    Complete exercise 13

---
+++ ex/ex13.py
@@ -0,0 +1,10 @@
+# LPTHW Exercise 13 -- Parameters, Unpacking, Variables
+
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is called:", first
+print "Your second variable is called:", second
+print "Your third variable is called:", third
