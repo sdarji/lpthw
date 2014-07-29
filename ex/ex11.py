407d69a0141ba603f472aa53754ffaef6fae28d0
Sachin Darji <sdarji@gmail.com>
2014-07-28 23:49:57 -0500

    Complete exercise 11

---
+++ ex/ex11.py
@@ -0,0 +1,10 @@
+# LPTHW Exercise 11 -- Asking Questions
+
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %s tall, and %r heavy." % (age, height, weight)
