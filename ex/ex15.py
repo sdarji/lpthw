d782de3b7503b116d63c69a4a6ce2812c40dc6f6
Sachin Darji <sdarji@gmail.com>
2014-07-29 21:38:50 -0500

    complete exercise 15

--- ex/ex15.py
+++ ex/ex15.py
@@ -7,7 +7,11 @@ script, filename = argv
 txt = open(filename)
 
 print "Here's your file %r:" % filename
-print txt.read()
+print txt.next()
+print txt.next()
+print txt.next()
+#print txt.read()
+txt.close()
 
 print "Type the filename again:"
 file_again = raw_input("> ")
@@ -15,3 +19,5 @@ file_again = raw_input("> ")
 txt_again = open(file_again)
 
 print txt_again.read()
+
+txt_again.close()
