4a9e5d5ab0a6b8489eaffc963266a7f24f92cd5e
Sachin Darji <sdarji@gmail.com>
2014-07-30 08:35:24 -0500

    completed exercise 17

--- ex/ex17.py
+++ ex/ex17.py
@@ -10,6 +10,7 @@ print "Copying from %s to %s" % (from_file, to_file)
 # we could do these two on one line too, how?
 in_file = open(from_file)
 indata = in_file.read()
+#indata = open(from_file).read()
 
 print "The input file is %d bytes long." % len(indata)
 
@@ -25,5 +26,7 @@ print "Alright, all done."
 out_file.close()
 in_file.close()
 
+# to do this in one line -->
+# from sys import argv; script, from_file, to_file = argv; open(to_file, 'w').write(open(from_file).read())
 
 
