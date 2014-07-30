1d10a3c0c1a8e5dacce03793a6b63c7346b78dd2
Sachin Darji <sdarji@gmail.com>
2014-07-30 09:08:38 -0500

    complete exercise 18

--- ex/ex18.py
+++ ex/ex18.py
@@ -2,7 +2,7 @@
 
 # this one is like your scripts with argv
 def print_two(*args):
-    arg1, arg2 = args
+    arg1, arg2 = argss
     print "arg1: %r, arg2: %r" % (arg1, arg2)
 
 # ok, that *args is actually pointless, we can just do this
