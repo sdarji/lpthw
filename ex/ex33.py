bb9d58e58e4d42f1a7a229b3b2c53a8d1e477b77
Sachin Darji <sdarji@gmail.com>
2014-07-31 02:00:52 -0500

    pass in max range and increment amount

--- ex/ex33.py
+++ ex/ex33.py
@@ -1,16 +1,19 @@
 # LPTHW Exercise 33 -- While-Loops
 
-i = 0
-numbers = []
+def number_loop(num, increment):
+    i = 0
+    numbers = []
+    while i < num:
+        print "At the top i is %d" % i
+        numbers.append(i)
+        i = i + increment
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
-    i = i + 1
-    print "Numbers now: ", numbers
-    print "At the bottom i is %d" % i
+    print "The numbers: "
+    for num in numbers:
+        print num,
 
-print "The numbers: "
-
-for num in numbers:
-    print num,
+number_loop(21, 3)
+number_loop(8, 2)
+number_loop(4, 1)
